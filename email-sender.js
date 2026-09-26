3900756bd646530da0b26722b96733f23951a8ca
bobzinou <erwan.92i@gmail.com>
2026-09-26 23:50:51 +0200

    Fix: Factures par mail

--- email-sender.js
+++ email-sender.js
@@ -5,11 +5,17 @@ const fs = require('fs');
 
 // ✅ Configure le transporteur Gmail
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS
-  }
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  },
+  family: 4,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 // ✅ Fonction pour envoyer la facture au CLIENT
